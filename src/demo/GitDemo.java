72587a0b0a788cc43fa54ab92fb80c9ccfd5967a
ABNER <ABNER@ABNER.lan>
2021-09-30 21:11:10 -0400

    updated user story

--- src/demo/GitDemo.java
+++ src/demo/GitDemo.java
@@ -3,6 +3,14 @@ package demo;
 public class GitDemo {
 	public static void main(String[] args) {
 		System.out.println("GitHub Prac");
+		
+		boolean isSunday = true;
+		if(isSunday) {
+			System.out.println("abc");
+			
+		}else {
+			System.out.println("Xyz");
+		}
 	}
 
 }
